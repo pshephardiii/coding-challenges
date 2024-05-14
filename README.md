c448bd481d8efc327854aee764b9ff9982a130ef
Paul Shephard <paulshephard598@gmail.com>
2024-05-14 14:21:21 -0400

    first challenge solved - solveMeFirst

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Keeping a record of coding challenges completed!
+
+## Challenge 1: solveMeFirst (Javascript)
+Complete function solveMeFirst to compute the sum of two integers
+Constraints: 1 <= a, b <= 1000
---
+++ script.js
@@ -0,0 +1,38 @@
+// Challenge One: solveMeFirst
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function solveMeFirst(a, b) {
+  // my code starts here
+  if (a >= 1 && a <= 1000 && b >= 1 && b <= 1000) {
+      return a + b
+  }
+  // my code ends here
+}
+
+
+function main() {
+    var a = parseInt(readLine());
+    var b = parseInt(readLine());;
+
+    var res = solveMeFirst(a, b);
+    console.log(res);
+}
