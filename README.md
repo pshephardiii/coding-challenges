87ced0d87be112823c20c11f05d9b34c959ba6a9
Paul Shephard <paulshephard598@gmail.com>
2024-05-14 20:42:14 -0400

    solved aVeryBigSum

--- README.md
+++ README.md
@@ -18,3 +18,7 @@ int b[3]: Bob's challenge rating
 Return
 
 int[2]: Alice's score is in the first position, and Bob's score is in the second.
+
+## Challenge 4: aVeryBigSum
+
+Return the sum of all array elements
--- script.js
+++ script.js
@@ -29,3 +29,13 @@ function compareTriplets(a, b) {
     }
     return finalArr
 }
+
+// Challenge 4
+
+function aVeryBigSum(ar) {
+    let sum = 0
+    for (let i = 0; i < ar.length; i++) {
+        sum = sum + ar[i]
+    }
+    return sum
+}
