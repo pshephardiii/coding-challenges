89d28b5330c92ba753410686f8a58555ef34ef22
Paul Shephard <paulshephard598@gmail.com>
2024-06-13 11:01:45 -0400

    recursion - factorial()

--- practice.js
+++ practice.js
@@ -1,8 +1,17 @@
-function power(num1, num2) {
-    if (num2 > 1) {
-        return num1 * power(num1, num2 - 1)
+// function power(num1, num2) {
+//     if (num2 > 1) {
+//         return num1 * power(num1, num2 - 1)
+//     }
+//     return num1
+// }
+
+// console.log(power(8, 0))
+
+function factorial(n) {
+    if (n > 1) {
+        return n * factorial(n - 1)
     }
-    return num1
+    console.log(n)
+    return n
 }
 
-console.log(power(8, 0))
