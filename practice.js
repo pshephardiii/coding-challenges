4f4eb89ab595c6ab888a893b45565d0cb14b7663
Paul Shephard <paulshephard598@gmail.com>
2024-06-13 11:28:50 -0400

    recursion - productOfArray()

--- practice.js
+++ practice.js
@@ -7,11 +7,35 @@
 
 // console.log(power(8, 0))
 
-function factorial(n) {
-    if (n > 1) {
-        return n * factorial(n - 1)
+// function factorial(n) {
+//     if (n > 1) {
+//         return n * factorial(n - 1)
+//     }
+//     console.log(n)
+//     return n
+// }
+
+function productOfArray(arr) {
+
+    if (arr.length === 0) {
+        return 'empty array'
     }
-    console.log(n)
-    return n
+    
+    let product = arr[arr.length - 1]
+
+    function multiplyArr(array) {
+
+        if (array.length > 1) {
+            array.pop()
+            product = product * array[array.length - 1]
+            return multiplyArr(array)
+        }
+        return product * array[0]
+    }
+
+    multiplyArr(arr)
+    return product
 }
+
+console.log(productOfArray([]))
 
