6dc41819c78548f62f85c40c8b596abaac49cc54
Paul Shephard <paulshephard598@gmail.com>
2024-06-16 15:28:09 -0400

    recursion - isPalindrome()

--- practice.js
+++ practice.js
@@ -91,6 +91,20 @@
 //     return revStr
 // }
 
+// function isPalindrome() {
+//     let arr = str.split('')
+//   if (arr.length === 1 || arr.length === 0) {
+//       return true
+//   }
+//   if (arr[0] === arr[arr.length - 1]) {
+//       arr.pop()
+//       arr.shift()
+//       let newStr = arr.join('')
+//       return isPalindrome(newStr)
+//   }
+//   return false
+// }
+
 // PROBLEM SOLVING PATTERNS
 
 // FREQUENCY COUNTER
@@ -121,30 +135,32 @@
 //     return true
 // }
 
-function validAnagram(str1, str2){
-    if (str1.split('').length !== str2.split('').length) {
-        return false
-    }
+// function validAnagram(str1, str2){
+//     if (str1.split('').length !== str2.split('').length) {
+//         return false
+//     }
     
-    let freqCounter1 = {}
-    let freqCounter2 = {}
+//     let freqCounter1 = {}
+//     let freqCounter2 = {}
     
-    for (let val of str1) {
-        freqCounter1[val] = (freqCounter1[val] || 0) + 1
-    }
-    for (let val of str2) {
-        freqCounter2[val] = (freqCounter2[val] || 0) + 1
-    }
+//     for (let val of str1) {
+//         freqCounter1[val] = (freqCounter1[val] || 0) + 1
+//     }
+//     for (let val of str2) {
+//         freqCounter2[val] = (freqCounter2[val] || 0) + 1
+//     }
     
-    for (let key in freqCounter1) {
-        if (freqCounter2[key]) {
-            if (freqCounter1[key] === freqCounter2[key]) {
-                continue
-            }
-        }
-        return false
-    }
-    return true
-  }
+//     for (let key in freqCounter1) {
+//         if (freqCounter2[key]) {
+//             if (freqCounter1[key] === freqCounter2[key]) {
+//                 continue
+//             }
+//         }
+//         return false
+//     }
+//     return true
+//   }
   
+// MULTIPLE POINTERS
+
 
