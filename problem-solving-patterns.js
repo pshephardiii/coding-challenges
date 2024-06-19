96e20260d1fe0774dd5f536cd1f0617f5bddc401
Paul Shephard <paulshephard598@gmail.com>
2024-06-19 19:26:58 -0400

    multiple pointers - averagePair()

--- problem-solving-patterns.js
+++ problem-solving-patterns.js
@@ -139,6 +139,26 @@
 //     return false
 //   }
 
+// function averagePair(arr, n){
+//     if (arr.length === 0) {
+//         return false
+//     }
+    
+//     let left = 0
+//     let right = arr.length - 1
+    
+//     while(left < right) {
+//         if ((arr[left] + arr[right]) / 2 === n) {
+//             return true
+//         } else if ((arr[left] + arr[right]) / 2 < n) {
+//             left++
+//         } else {
+//             right--
+//         }
+//     }
+//     return false
+// }
+
 // SLIDING WINDOW
 
 // function maxSubarraySum(arr, n){
