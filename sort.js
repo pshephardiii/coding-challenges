0c7436ecd03c81d0723c19f9e9cb1ee27f2309c2
Paul Shephard <paulshephard598@gmail.com>
2024-06-22 17:14:41 -0400

    sorting - bubbleSort()

---
+++ sort.js
@@ -0,0 +1,25 @@
+// Bubble Sort
+
+// function bubbleSort(arr) {
+//     // use to check for whether there were any swaps on the iteration...
+//     // if not, array is already sorted, so break
+//     let noSwaps = null
+
+//     for (let i = arr.length - 1; i >= 0; i--) {
+
+//         noSwaps = true
+
+//         for (let j = 0; j <= i - 1; j++) {
+
+//             if (arr[j] > arr[j + 1]) {
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+//                 noSwaps = false
+//             }
+//         }
+//         if (noSwaps) {
+//             break
+//         } 
+//     }
+//     return arr
+// }
+
