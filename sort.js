e11be3dc0d3c8a66bcd6163592d350518e40c6e1
Paul Shephard <paulshephard598@gmail.com>
2024-06-22 17:45:14 -0400

    sorting - selectionSort()

--- sort.js
+++ sort.js
@@ -23,3 +23,24 @@
 //     return arr
 // }
 
+// Selection Sort
+
+// function selectionSort(arr) {
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         let min = i
+
+//         for (let j = i + 1; j < arr.length; j++) {
+
+//             if (arr[j] < arr[min]) {
+//                 min = j
+//             }
+//         }
+//         if (i !== min) {
+//             [arr[i], arr[min]] = [arr[min], arr[i]]
+//         }
+//     }
+//     return arr
+// }
+
