419a2b2497c6c0e49fd257ce6054b0dbfb8047e3
Paul Shephard <paulshephard598@gmail.com>
2024-06-23 11:41:40 -0400

    sorting - insertionSort()

--- sort.js
+++ sort.js
@@ -44,3 +44,18 @@
 //     return arr
 // }
 
+// Insertion Sort
+
+// function insertionSort(arr) {
+
+//     for (let i = 1; i < arr.length; i++) {
+        
+//         let currentVal = arr[i]
+
+//         for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+//             arr[j + 1] = arr[j]
+//             arr[j] = currentVal
+//         }
+//     }
+//     return arr
+// }
