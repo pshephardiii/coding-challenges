fda1f8604c1080fabcaffd59b0d00fb2730a46d2
Paul Shephard <paulshephard598@gmail.com>
2024-06-24 15:54:30 -0400

    sorting - mergeSort()

--- sort.js
+++ sort.js
@@ -59,3 +59,60 @@
 //     }
 //     return arr
 // }
+
+// Merge Sort
+
+// Merge helper function
+// function merge(n, m) {
+//     let arr = []
+//     let i = 0
+//     let j = 0
+
+//     while (arr.length < (n.length + m.length)) {
+//         // if the n value is greater than the m value, push that m value and increase the index for m
+//         if (n[i] > m[j]) {
+//             arr.push(m[j])
+//             j++
+//         // if the n value is less, push that n value and increase the index for n
+//         } else if (n[i] < m[j]) {
+//             arr.push(n[i])
+//             i++
+//         // if the values are equal, push both and increase both indexes
+//         } else if (n[i] === m[j]) {
+//             arr.push(n[i])
+//             arr.push(m[j])
+//             i++
+//             j++
+//         // This else will run if one of the arrays is empty but the other is not
+//         } else {
+//             // if there's anything left in n, push it and increase index
+//             if (i < n.length) {
+//                 arr.push(n[i])
+//                 i++
+//             // if there's anything left in m, push it and increase index
+//             } else if (j < m.length) {
+//                 arr.push(m[j])
+//                 j++
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+// function mergeSort(arr) {
+//     if (arr.length <= 1) {
+//         return arr
+//     }
+//     // find midpoint of array
+//     let mid = Math.floor(arr.length / 2)
+//     // create shallow copy of left half of array
+//     // send that left half back into the function to be broken up further
+//     let left = mergeSort(arr.slice(0, mid))
+//     let right = mergeSort(arr.slice(mid))
+//     // Eventually merge the array elements into the right order piece by piece
+//     return merge(left, right)
+// }
+
+
+
+
