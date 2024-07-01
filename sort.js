b46d53304fc9255c64246e6792e0768cf5ad6710
Paul Shephard <paulshephard598@gmail.com>
2024-07-01 19:03:28 -0400

    sorting: quick sort

--- sort.js
+++ sort.js
@@ -115,6 +115,36 @@
 
 // Quick Sort
 
+// function quickSort(arr, left = 0, right = arr.length - 1){
+//     if(left < right) {
+//         let pivotIndex = pivot(arr, left, right)
+//         // left
+//         quickSort(arr, left, pivotIndex - 1)
+//         // right
+//         quickSort(arr, pivotIndex + 1, right)
+//     }
+//     return arr
+// }
 
+// function pivot(arr, start, end){
+
+//     function swap(array, i, j){
+//         let temp = array[i]
+//         array[i] = array[j]
+//         array[j] = temp
+//     }
+
+//     let pivot = arr[start]
+//     let swapIdx = start
+
+//     for(i = start + 1; i < arr.length; i++) {
+//         if(pivot > arr[i]){
+//             swapIdx++
+//             swap(arr, swapIdx, i)
+//         }
+//     }
+//     swap(arr, start, swapIdx)
+//     return swapIdx
+// }
 
 
