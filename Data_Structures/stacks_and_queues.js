40c5327dff44b4c2b25bb24c608599723e965ef8
Paul Shephard <paulshephard598@gmail.com>
2024-07-04 14:10:57 -0400

    data structures: singly linked list as a stack

--- Data_Structures/stacks_and_queues.js
+++ Data_Structures/stacks_and_queues.js
@@ -7,6 +7,11 @@
 
 // Use cases: managing function invocations, undo/redo, routing (history object), algorithms for trees and graphs
 
+// Big O:
+// Insertion: O(1)
+// Removal: 0(1)
+// searching and access: O(N) - typically don't use stacks for these
+
 // Making a stack out of an array:
 
 let stack = []
@@ -34,4 +39,51 @@ stack2.shift()
 // We should ALWAYS use the push/pop method for efficiency's sake
 
 // Also, if you are worried about efficiency, arrays aren't the way to go for stacks, as they store indices needlessly
+
+// Stack as a singly linked list:
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+    // this is really unshift!
+    push(val){
+        let newNode = new Node(val)
+        if (!this.first){
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let temp = this.first
+            this.first = newNode
+            this.first.next = temp
+        }
+        return ++this.size
+    }
+    // this is really shift!
+    pop(){
+        let prevFirst = this.first
+        if (!this.first) {
+            return null
+        } else if (this.first === this.last) {
+            this.last = null
+        } 
+        this.first = this.first.next
+        this.size--
+        return prevFirst.val
+    }
+}
+
+
+
+
+
 
