49cc09273b49bfc2015297d0789df8c698935ceb
Paul Shephard <paulshephard598@gmail.com>
2024-07-04 12:16:36 -0400

    doubly linked lists: unshift method

--- Data_Structures/doubly_linked_lists.js
+++ Data_Structures/doubly_linked_lists.js
@@ -64,6 +64,20 @@ class DoublyLinkedList{
         this.length--
         return prevHead
     }
+
+    unshift(val){
+        let newNode = new Node(val)
+        if (!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
 }
 
 let list = new DoublyLinkedList()
