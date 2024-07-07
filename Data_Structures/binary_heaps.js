9c3ac041f6350f1e3b4cc1d2d6fe07b1bc9357d2
Paul Shephard <paulshephard598@gmail.com>
2024-07-07 17:44:54 -0400

    binary heaps: insert method

--- Data_Structures/binary_heaps.js
+++ Data_Structures/binary_heaps.js
@@ -13,3 +13,46 @@
 // used to implement priority queue
 // also used with graph traversal algorithms
 
+// we can use arrays/lists to store heaps
+
+// for any index of an array n, the left child is stored at 2n + 1 and the right child is stored at 2n + 2
+
+// for any index of an array n, the parent is at index Math.floor((n - 1) / 2)
+
+// Binary Heap Class:
+
+class MaxBinaryHeap {
+    constructor(){
+        this.vals = []
+    }
+
+    // add to end, bubble up
+
+    insert(val) {
+        this.vals.push(val)
+        let index = this.vals.length - 1
+        let parentIndex = Math.floor((index - 1) / 2)
+        // Keep swapping the new value with its parent until the parent is greater
+        while(this.vals[index] > this.vals[parentIndex]) {
+            let temp = this.vals[index]
+            this.vals[index] = this.vals[parentIndex]
+            this.vals[parentIndex] = temp
+            index = parentIndex
+            parentIndex = Math.floor((index - 1) / 2)
+        }
+        return this
+    }
+
+    // extractMax() {
+
+    // }
+}
+
+let heap = new MaxBinaryHeap()
+console.log(heap.insert(1))
+console.log(heap.insert(2))
+console.log(heap.insert(40))
+console.log(heap.insert(3))
+console.log(heap.insert(33))
+console.log(heap.insert(35))
+console.log(heap.insert(100))
