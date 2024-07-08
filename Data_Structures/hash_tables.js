d2073dacd812ec89e5bdd9aa66a31ca4aec5a8f2
Paul Shephard <paulshephard598@gmail.com>
2024-07-08 10:51:18 -0400

    hash tables: intro to hash functions

--- Data_Structures/hash_tables.js
+++ Data_Structures/hash_tables.js
@@ -13,3 +13,42 @@
 
 // Hash Functions
 
+// some hash functions are one-way, in that the output doesn't tell you anything on its own
+
+// What makes a good hash? (not a crytpographically secure one)
+// Fast (constant time)
+// Distributes uniformly (doesn't cluster outputs at specific indices)
+// Deterministic (same input yields same output)
+
+// char code UTF values: "a".charCodeAt(0)... if you subtract by 96, you could alphabetic positioning (example "a" would be 1)
+
+// Hash that works on strings only:
+
+function hash(key, arrayLen) {
+    let total = 0
+    for (let char of key) {
+        // the value will be the UTF code of char - 96
+        let value = char.charCodeAt(0) - 96
+        total = (total + value) % arrayLen
+    }
+    return total
+}
+
+// problem: not constant time and could make data clustered
+
+// an improved (still not great) hash that uses prime numbers:
+
+function hash(key, arrayLen) {
+    let total = 0
+    let prime = 31
+    // put a cap on the number of times we loop even if key has more than 100 characters
+    for (let i = 0; i< Math.min(key.length, 100); i++) {
+        let char = key[i]
+        let value = char.charCodeAt(0) - 96
+        total = (total * prime + value) % arrayLen
+    }
+    return total
+}
+
+// handling collisions:
+
