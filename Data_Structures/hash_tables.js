34e2da6b8ff9349e8d3c4e50a06ebe0fedc733ab
Paul Shephard <paulshephard598@gmail.com>
2024-07-08 12:24:27 -0400

    hash tables: keys() and values()

--- Data_Structures/hash_tables.js
+++ Data_Structures/hash_tables.js
@@ -15,6 +15,9 @@
 
 // some hash functions are one-way, in that the output doesn't tell you anything on its own
 
+// Big O:
+// insert, deletion, access: O(1) - average case
+
 // What makes a good hash? (not a crytpographically secure one)
 // Fast (constant time)
 // Distributes uniformly (doesn't cluster outputs at specific indices)
@@ -63,3 +66,89 @@ function hash(key, arrayLen) {
 // when we find a collision, we search through the array to find the next empty slot
 
 // We'll use separate chaining
+
+// Hash Table Class:
+
+class HashTable {
+    constructor(size=53) {
+        this.keyMap = new Array(size)
+    }
+
+    _hash(key) {
+        let total = 0
+        let prime = 31
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * prime + value) % this.keyMap.length
+        }
+        return total
+    }
+
+    set(key, value) {
+        let index = this._hash(key)
+        let data = [key, value]
+        // if the indexed spot is itself an array, that means there is data already in it, so we can just push the new data in as a separate array
+        // if it's not an array, just set the indexed spot to an array that has the data as a subarray
+        Array.isArray(this.keyMap[index]) ? this.keyMap[index].push(data) : 
+        this.keyMap[index] = [data]
+        return this.keyMap
+    }
+
+    get(key) {
+        let index = this._hash(key)
+        let mapIndex = this.keyMap[index]
+        // check if it is array to avoid error
+        if (Array.isArray(mapIndex)) {
+            // if it is array, loop through and look for a subarray that includes the key
+            for (let i = 0; i < mapIndex.length; i ++) {
+                if (mapIndex[i][0] === key) {
+                    return mapIndex[i][1]
+                }
+            }
+        }
+        // if the indexed spot is empty or doesn't contain a subarray with the key, return undefined
+        return undefined
+    }
+
+    keys() {
+        let result = []
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (!this.keyMap[i]) {
+                continue
+            }
+            for (let j = 0; j < this.keyMap[i].length; j++) {
+                // use this conditional if you only want to return unique keys/values
+                if (!result.includes(this.keyMap[i][j][0])) {
+                    result.push(this.keyMap[i][j][0])
+                }
+            }
+        }
+        return result
+    }
+
+    values() {
+        let result = []
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (!this.keyMap[i]) {
+                continue
+            }
+            for (let j = 0; j < this.keyMap[i].length; j++) {
+                if (!result.includes(this.keyMap[i][j][1])) {
+                    result.push(this.keyMap[i][j][1])
+                }
+            }
+        }
+        return result
+    }
+}
+
+let table = new HashTable()
+
+table.set("red", 77)
+table.set("red", 77)
+table.set("pink", 3)
+console.log(table.set("oh boy", 8000))
+console.log(table.get("pink"))
+console.log(table.keys())
+console.log(table.values())
