29d835c72a363edec347136ca62b240052a6d8e2
Paul Shephard <paulshephard598@gmail.com>
2024-07-09 11:50:28 -0400

    graphs: remove edge method

--- Data_Structures/graphs.js
+++ Data_Structures/graphs.js
@@ -74,11 +74,24 @@ class Graph {
             this.adjacencyList[vertex2].push(vertex1)
         }
     }
+
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+                v => v !== vertex2
+            )
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+                v => v !== vertex1
+            )
+        }
+    }
 }
 
 let graph = new Graph()
 graph.addVertex('fun')
 graph.addVertex('times')
+graph.addVertex('life')
 graph.addEdge('fun', 'times')
-
+graph.addEdge('fun', 'life')
+graph.removeEdge('fun', 'times')
 console.log(graph)
