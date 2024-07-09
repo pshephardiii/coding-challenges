d76a1a6517e58b1ca1f57cae90773fce91448dfb
Paul Shephard <paulshephard598@gmail.com>
2024-07-09 11:29:11 -0400

    graphs: adjacency list vs matrix

--- Data_Structures/graphs.js
+++ Data_Structures/graphs.js
@@ -19,4 +19,38 @@
 // Weighted graph: edges have a value associated to them
 // Unweighted: edges have no value
 
+// Storing graphs:
 
+// Method 1: Adjacency Matrix
+// Nested arrays... rows and columns
+// So, you have a row and column for each node.  When the nodes connect (Like row A and column B), you put a 1.  If not, 0.
+
+// Below: A is connected to B and D, but B and D are not connected.  B is also connected to C, which is not connected to any other node.  This kind of diagram would be represented as a 2d array. *** this is undirected graph ***
+
+//  | A | B | C | D |
+// A| 0 | 1 | 0 | 1 |
+// B| 1 | 0 | 1 | 0 |
+// C| 0 | 1 | 0 | 0 |
+// D| 1 | 0 | 0 | 0 |
+
+// Method 2: Adjacency List
+
+// use a list to store edges
+// Use hash table to list each node as a key and their values as the nodes they are connected to
+
+// Big O: Matrix vs List
+// *** V is for number of vertices, E is number of edges
+// Add vertex: O(1) for List, O(V^2) for Matrix
+// Add edges: both O(1)
+// Remove vertex: O(V + E) vs O(V^2)
+// Remove edge: O(E) vs O(1)
+// Query: O(V + E) vs O(1)
+// Storage: O(V + E) vs O(V^2)
+
+// if you have a lot of edges, matrix can be good, but list is better in terms of number of vertices
+
+// List: can take up less space, faster to iterate over all edges, can be slower to lookup a specific edge
+
+// Matrix: takes up more space, slower to iterate over all edges, faster to lookup specific edge
+
+// For our purposes, we'll use the adjacency list, as data tends to be sparse in the real world
