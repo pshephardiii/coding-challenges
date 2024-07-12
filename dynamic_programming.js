b8ac867d8a637ef78828077f7ce40d783c0129d8
Paul Shephard <paulshephard598@gmail.com>
2024-07-12 11:05:34 -0400

    dynamic programming: overlapping subproblems

--- dynamic_programming.js
+++ dynamic_programming.js
@@ -4,3 +4,13 @@
 
 // "dynamic programming" is a very misleading terms... just focus on the above
 
+// only useful with an optimal substructure and overlapping subproblems
+
+// OVERLAPPING SUBPROBLEMS
+
+// a problem that can be broken down into subproblems which are reused several times
+// example: fibonacci sequenece
+// if you want fib(5), you need to add fib(4) and fib(3).  However, to get fib(4), you need to calculate fib(3) as it is part of the sum that results in fib(4).  So, you'd have to calculate fib(3) more than once, thus the overlapping of the subproblems
+// MergeSort... no overlapping subproblems (unless there are exact repeating halves)
+
+// OPTIMAL SUBSTRUCTURE
