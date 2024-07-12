1a52dcaef396b1786b087e98cf1f427ad7c7c9c3
Paul Shephard <paulshephard598@gmail.com>
2024-07-12 10:59:22 -0400

    dynamic programming: basics

---
+++ dynamic_programming.js
@@ -0,0 +1,6 @@
+// just a light introduction
+
+// method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions
+
+// "dynamic programming" is a very misleading terms... just focus on the above
+
