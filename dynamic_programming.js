3f766d78e1e9a5450ad65b35ed41cd4693ac140a
Paul Shephard <paulshephard598@gmail.com>
2024-07-12 11:12:03 -0400

    dynamic programming: optimal substructure

--- dynamic_programming.js
+++ dynamic_programming.js
@@ -14,3 +14,11 @@
 // MergeSort... no overlapping subproblems (unless there are exact repeating halves)
 
 // OPTIMAL SUBSTRUCTURE
+
+// a problem where the optimal solution can be constructed from optimal solutions of its subproblems
+// ex. fibonnaci
+// to get fib(5), you need the optimal solutions for fib(4), fib(3), etc... that is, you can use the optimal solutions of the subproblems to get the solution for the problem itself
+
+// another example: shortest path
+// To get the shortest path from A to E, you need to start by finding the shortest path to, say, A to B, then B to some other node, ending at E.  It works like the loop in Dijkstra's algorith... to find the shortest overall path, need to find shortest path for every step along the way
+
